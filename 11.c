92884f4acae58142536c06cf2ab96bc56bc0139c
aashikarajasekar <35651549+aashikarajasekar@users.noreply.github.com>
2018-03-07 15:52:26 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+char a[30];
+printf("enter the sting");
+scanf("%s",&a);
+switch(a)
+{
+case 'monday':
+printf("its not holiday");
+break;
+case 'tuesday':
+printf("its not holiday");
+break;
+case 'wednesday':
+printf("its not holiday");
+break;
+case 'thursday':
+printf("its not holiday");
+break;
+case 'friday':
+printf("its not holiday");
+break;
+case 'saturday':
+printf("its  holiday");
+break;
+case 'sunday':
+printf("its  holiday");
+break;
+}
